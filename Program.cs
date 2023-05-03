baebabdc90ad0e7fbf6b89a275c9fc1c7881ece0
kananbayramli <bayramli-kenan@mail.ru>
2023-05-03 23:30:35 +0400

    Enum basic

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System;
+using Enum_ders.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Enum_ders
+{
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var properties = typeof(ShippingStatus).GetFields();
+            foreach (var prop in properties)
+            {
+                var dd = prop.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+                if (dd != null)
+                {
+                    Console.WriteLine(prop.Name + " --> " + dd.Name);
+                }
+            }
+
+            //------------[Flags]-------------//
+
+
+            Console.WriteLine(
+                 "\n\nAll possible combinations of values without FlagsAttribute:");
+            for (int val = 0; val <= 16; val++)
+                Console.WriteLine("{0,3} - {1:G}", val, (SingleHue)val);
+
+
+
+            Console.WriteLine(
+                 "\nAll possible combinations of values with FlagsAttribute:");
+            for (int val = 0; val <= 16; val++)
+                Console.WriteLine("{0,3} - {1:G}", val, (MultiHue)val);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShippingStatus.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Enum_ders.Models
+{
+
+
+
+    public enum ShippingStatus : byte
+    {
+        None = 0,
+
+        [Display(Name = "Aktif Kullanıcıları Listeler")]
+        Active = 1,
+
+
+        [Display(Name = "Pasif Kullanıcıları Listeler")]
+        Passive,
+
+
+        [Display(Name = "Silinmiş Kullanıcıları Listeler")]
+        Deleted,
+
+
+        [Display(Name = "Kaydını Dondurmuş Kullanıcıları Listeler")]
+        Holiday
+    }
+
+    enum SingleHue : short
+    {
+        None = 0,
+        Black = 1,
+        Red = 2,
+        Green = 4,
+        Blue = 8
+    };
+
+
+    [Flags]
+    enum MultiHue : short
+    {
+        None = 0,
+        Black = 1,
+        Red = 2,
+        Green = 4,
+        Blue = 8
+    };
+
+}
